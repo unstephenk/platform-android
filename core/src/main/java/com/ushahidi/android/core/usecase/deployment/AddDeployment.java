792e4014abae6befafc0efc0762b58687ebae36b
Henry Addo <addhen@linuxmail.org>
2014-09-09 08:56:47 +0900

    Add use case for adding a deployment

---
+++ core/src/main/java/com/ushahidi/android/core/usecase/deployment/AddDeployment.java
@@ -0,0 +1,15 @@
+/**
+ * AddDeployment Implements Add Deployment Use Case
+ *
+ * @author Ushahidi Team <team@ushahidi.com>
+ * @package com.ushahidi.android.core.usecase.deployment
+ * @copyright 2014 Ushahidi
+ * @license https://www.gnu.org/licenses/agpl-3.0.html GNU Affero General Public License Version 3 (AGPL3)
+ */
+package com.ushahidi.android.core.usecase.deployment;
+
+import com.ushahidi.android.core.usecase.Interactor;
+
+public interface AddDeployment extends Interactor {
+
+}
