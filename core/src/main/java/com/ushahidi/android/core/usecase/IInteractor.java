59c0376d8a7116c164ed7c0a140cabde2dea5a80
Henry Addo <addhen@linuxmail.org>
2014-09-09 09:44:29 +0900

    Update copright info. Add it at the header of the file

--- core/src/main/java/com/ushahidi/android/core/usecase/IInteractor.java
+++ core/src/main/java/com/ushahidi/android/core/usecase/IInteractor.java
@@ -1,10 +1,18 @@
-/**
- * Interactor, an interface for executing the different use cases
+/*
+ * Copyright (c) 2014 Ushahidi.
  *
- * @author Ushahidi Team <team@ushahidi.com>
- * @package com.ushahidi.android.domain.usecase
- * @copyright 2014 Ushahidi
- * @license https://www.gnu.org/licenses/agpl-3.0.html GNU Affero General Public License Version 3 (AGPL3)
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program in the file LICENSE-AGPL. If not, see
+ * https://www.gnu.org/licenses/agpl-3.0.html
  */
 package com.ushahidi.android.core.usecase;
 
--- core/src/main/java/com/ushahidi/android/core/usecase/deployment/IAddDeployment.java
+++ core/src/main/java/com/ushahidi/android/core/usecase/deployment/IAddDeployment.java
@@ -1,15 +1,26 @@
-/**
- * AddDeployment Implements Add Deployment Use Case
+/*
+ * Copyright (c) 2014 Ushahidi.
  *
- * @author Ushahidi Team <team@ushahidi.com>
- * @package com.ushahidi.android.core.usecase.deployment
- * @copyright 2014 Ushahidi
- * @license https://www.gnu.org/licenses/agpl-3.0.html GNU Affero General Public License Version 3 (AGPL3)
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program in the file LICENSE-AGPL. If not, see
+ * https://www.gnu.org/licenses/agpl-3.0.html
  */
 package com.ushahidi.android.core.usecase.deployment;
 
 import com.ushahidi.android.core.usecase.IInteractor;
 
+/**
+ * AddDeployment Implements Add Deployment Use Case
+ */
 public interface IAddDeployment extends IInteractor {
 
 }
