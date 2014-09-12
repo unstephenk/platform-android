a269d255b5ec2402753f451b543e6a9fe7df24ff
Henry Addo <addhen@linuxmail.org>
2014-09-12 14:18:30 +0900

    Set base classes for loading data into views

---
+++ app/src/main/java/com/ushahidi/android/ui/view/LoadViewData.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2014 Ushahidi.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program in the file LICENSE-AGPL. If not, see
+ * https://www.gnu.org/licenses/agpl-3.0.html
+ */
+
+package com.ushahidi.android.ui.view;
+
+import android.content.Context;
+
+/**
+ * Implement this interface to load data into the view
+ *
+ * @author Ushahidi Team <team@ushahidi.com>
+ */
+public interface LoadViewData {
+
+    /**
+     * Get a {@link android.content.Context}.
+     */
+    public Context getContext();
+
+    /**
+     * Show a view with a progress bar indicating a loading process.
+     */
+    public void showLoading();
+
+    /**
+     * Hide a loading view.
+     */
+    public void hideLoading();
+
+    /**
+     * Show a retry view in case of an error when retrieving data.
+     */
+    public void showRetry();
+
+    /**
+     * Hide a retry view shown if there was an error when retrieving data.
+     */
+    public void hideRetry();
+
+    /**
+     * Show an error message
+     *
+     * @param message A string representing an error.
+     */
+    public void showError(String message);
+
+}
