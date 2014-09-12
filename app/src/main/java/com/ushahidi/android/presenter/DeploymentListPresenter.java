90e9a05bb31f3080933a1ad3b6578bc5a4028205
Henry Addo <addhen@linuxmail.org>
2014-09-12 14:08:21 +0900

    Add initial presenter implementation

---
+++ app/src/main/java/com/ushahidi/android/presenter/DeploymentListPresenter.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2014 Ushahidi.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program in the file LICENSE-AGPL. If not, see
+ * https://www.gnu.org/licenses/agpl-3.0.html
+ */
+
+package com.ushahidi.android.presenter;
+
+/**
+ * {@link com.ushahidi.android.presenter.Presenter} facilitates interactions between deployment
+ * list view and deployment models.
+ *
+ * @author Ushahidi Team <team@ushahidi.com>
+ */
+public class DeploymentListPresenter implements Presenter {
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+}
